aa033664fcaffd85f43ed8f923bafd0315b0d5cd
quentingruber <quentingruber@gmail.com>
2024-10-21 17:01:11 +0200

    split officials and community servers

--- src/routes/ServerList.svelte
+++ src/routes/ServerList.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import axios from "axios";
-  let servers: any[] = [];
+  let officialServers: any[] = [];
+  let communityServers: any[] = [];
   async function updateServersData() {
     try {
       let req = await axios.get("http://loginserver.h1emu.com/servers");
       const serverData = req.data.filter((e: any) => !e.isDisabled);
-      servers = serverData;
+      officialServers = serverData.filter((e: any) => e.IsOfficial);
+      communityServers = serverData.filter((e: any) => !e.IsOfficial);
     } catch (e) {
       console.error(e);
     }
@@ -16,19 +18,57 @@
 <table class="min-w-full divide-y divide-gray-200">
   <thead class="bg-gray-50">
     <tr>
-      <th class="px-6 py-4 whitespace-nowrap text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Server Name</th>
-      <th class="px-6 py-4 whitespace-nowrap text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Population</th>
-      <th class="px-6 py-4 whitespace-nowrap text-left text-xs font-medium text-gray-500 uppercase tracking-wider">H1emu Version</th>
-      <th class="px-6 py-4 whitespace-nowrap text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
+      <th
+        class="px-6 py-4 whitespace-nowrap text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+        >Server Name</th
+      >
+      <th
+        class="px-6 py-4 whitespace-nowrap text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+        >Population</th
+      >
+      <th
+        class="px-6 py-4 whitespace-nowrap text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+        >H1emu Version</th
+      >
+      <th
+        class="px-6 py-4 whitespace-nowrap text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+        >Status</th
+      >
     </tr>
   </thead>
   <tbody class="bg-white divide-y divide-gray-200">
-    {#each servers as server}
+    <h2>Officials servers</h2>
+    {#each officialServers as server}
       <tr class="hover:bg-gray-100">
-        <td class="px-6 py-4 whitespace-nowrap">{server.name} ({server.region}) #{server.serverId}</td>
-        <td class="px-6 py-4 whitespace-nowrap">{server.populationNumber}/{server.maxPopulationNumber}</td>
-        <td class="px-6 py-4 whitespace-nowrap">h1z1-server {server.h1emuVersion}</td>
-        <td class="px-6 py-4 whitespace-nowrap">{server.locked ? "locked" : server.allowedAccess ? "Up" : "Down"}</td>
+        <td class="px-6 py-4 whitespace-nowrap"
+          >{server.name} ({server.region}) #{server.serverId}</td
+        >
+        <td class="px-6 py-4 whitespace-nowrap"
+          >{server.populationNumber}/{server.maxPopulationNumber}</td
+        >
+        <td class="px-6 py-4 whitespace-nowrap"
+          >h1z1-server {server.h1emuVersion}</td
+        >
+        <td class="px-6 py-4 whitespace-nowrap"
+          >{server.locked ? "locked" : server.allowedAccess ? "Up" : "Down"}</td
+        >
+      </tr>
+    {/each}
+    <h2>Community servers</h2>
+    {#each communityServers as server}
+      <tr class="hover:bg-gray-100">
+        <td class="px-6 py-4 whitespace-nowrap"
+          >{server.name} ({server.region}) #{server.serverId}</td
+        >
+        <td class="px-6 py-4 whitespace-nowrap"
+          >{server.populationNumber}/{server.maxPopulationNumber}</td
+        >
+        <td class="px-6 py-4 whitespace-nowrap"
+          >h1z1-server {server.h1emuVersion}</td
+        >
+        <td class="px-6 py-4 whitespace-nowrap"
+          >{server.locked ? "locked" : server.allowedAccess ? "Up" : "Down"}</td
+        >
       </tr>
     {/each}
   </tbody>
